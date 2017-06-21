6c59d63081e94713e91d8e9a70ffe9fc03dc9e2c
Peter T <ptumulty923@gmail.com>
2017-06-20 20:40:41 -0400

    added files

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+
+          function changeColor(){
+             $('#colors li').on('click', function(){
+               var color = $(this).data('color');
+               var animations = [
+                 'animated bounce',
+                 'animated jello',
+                 'animated rubberBand',
+                 'animated rotateIn',
+                 'animated zoomIn',
+                 'animated pulse'
+               ];
+
+               var selectedAnimation = animations[Math.floor(Math.random()*animations.length)];
+               console.log(selectedAnimation);
+               $(this).addClass(selectedAnimation);
+
+                if(color === 'gray'){
+                  $('body').css('background-color', '#e9e9e9');
+                }else{
+                  $('body').css('background-color', color);
+                }
+
+             });
+          };
+
+          $(document).ready(changeColor);
